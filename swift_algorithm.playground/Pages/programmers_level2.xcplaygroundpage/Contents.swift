90daf3450dbb5ec9f72b518bdc5ca75b6a628e68
lyk-brave <officeyongki@gmail.com>
2020-10-16 08:48:54 +0900

    2020-10-16 08:48

--- swift_algorithm.playground/Pages/programmers_level2.xcplaygroundpage/Contents.swift
+++ swift_algorithm.playground/Pages/programmers_level2.xcplaygroundpage/Contents.swift
@@ -699,3 +699,20 @@ func solution17679(_ m:Int, _ n:Int, _ board:[String]) -> Int {
     return cnt
 }
 
+/// - 예상 대진표
+/// - https://programmers.co.kr/learn/courses/30/lessons/12985?language=swift
+func solution12985(_ n:Int, _ a:Int, _ b:Int) -> Int {
+    var ab: (a: Int, b: Int) = (a, b)
+    var cnt: Int = 1
+    while true {
+        if (ab.a.isMultiple(of: 2) && ab.a - 1 == ab.b) || (!ab.a.isMultiple(of: 2) && ab.a + 1 == ab.b) {
+            break
+        }
+        ab.a = ab.a % 2 == 0 ? ab.a / 2 : ab.a / 2 + 1
+        ab.b = ab.b % 2 == 0 ? ab.b / 2 : ab.b / 2 + 1
+        cnt += 1
+    }
+    
+    return cnt
+}
+
